6ee37e732ecb96ffc73b47479f0a366fcb00ca90
gamejam <gamejam@coleman.local>
2017-01-22 01:48:27 -0800

    Updating

---
+++ GGJ17Alpha/Source/GGJ17Alpha/GGJ17Alpha.cpp
@@ -0,0 +1,10 @@
+// Copyright 1998-2016 Epic Games, Inc. All Rights Reserved.
+
+#include "GGJ17Alpha.h"
+
+
+IMPLEMENT_PRIMARY_GAME_MODULE(FDefaultGameModuleImpl, GGJ17Alpha, "GGJ17Alpha");
+
+DEFINE_LOG_CATEGORY(LogFlying)
+
+ 
