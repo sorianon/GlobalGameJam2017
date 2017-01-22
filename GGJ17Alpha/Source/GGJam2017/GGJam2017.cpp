a5e10499fbca981683977fa9e46fceeff8e21102
Ashley Hearne <Ashley Hearne>
2017-01-22 03:41:48 -0800

    my stuff

---
+++ GGJ17Alpha/Source/GGJam2017/GGJam2017.cpp
@@ -0,0 +1,10 @@
+// Copyright 1998-2016 Epic Games, Inc. All Rights Reserved.
+
+#include "GGJam2017.h"
+
+
+IMPLEMENT_PRIMARY_GAME_MODULE(FDefaultGameModuleImpl, GGJam2017, "GGJam2017");
+
+DEFINE_LOG_CATEGORY(LogFlying)
+
+ 
